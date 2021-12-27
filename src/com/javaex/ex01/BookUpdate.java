5abb52c58e7653a7d943bdce8feb35665221d13a
sihoney <hwangheewon@naver.com>
2021-12-27 22:43:37 +0900

    jdbc - insert/update/delete

---
+++ src/com/javaex/ex01/BookUpdate.java
@@ -0,0 +1,66 @@
+package com.javaex.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookUpdate {
+   public static void main(String[] args) {
+      // 0. import java.sql.*;
+      Connection conn = null;
+      PreparedStatement pstmt = null;
+      ResultSet rs = null;
+      
+      try {
+         // 1. JDBC 드라이버 (Oracle) 로딩
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         
+         // 2. Connection 얻어오기
+         String url = "jdbc:oracle:thin:@localhost:1521:xe";
+         conn = DriverManager.getConnection(url, "webdb", "webdb");
+         
+         // 3. SQL문 준비 / 바인딩 / 실행
+         String query = "";
+         query += " update book ";
+         query += " set pubs = ?, ";
+         query += "     pub_date = ? ";
+         query += " where book_id = ? ";
+         
+         pstmt = conn.prepareStatement(query);
+         
+         pstmt.setString(1, "다림출판");
+         pstmt.setString(2, "2000-02-22");
+         pstmt.setInt(3, 3);
+         
+         int count = pstmt.executeUpdate();
+         
+         // 4.결과처리
+         System.out.println(count + "건이 수정되었습니다.");
+         
+      } catch (ClassNotFoundException e) {
+         System.out.println("error: 드라이버 로딩 실패 - " + e);
+         
+      } catch (SQLException e) {
+         System.out.println("error:" + e);
+         
+
+      } finally {
+         // 5. 자원정리
+         try {
+            if (rs != null) {
+               rs.close();
+            }
+            if (pstmt != null) {
+               pstmt.close();
+            }
+            if (conn != null) {
+               conn.close();
+            }
+         } catch (SQLException e) {
+            System.out.println("error:" + e);
+         }
+      }
+   }
+}
